8f9a7491679ac120f8d9186459e7188fe2292194
raddcircles <raddcircles@gmail.com>
2018-01-22 21:24:25 -0500

    Fix some stuff

--- post.py
+++ post.py
@@ -23,7 +23,8 @@ def login():
         print(colored("Invalid Twitter Credentials!", "red"))
         os.system("pause>nul")
         sys.exit()
-
+twitter = login()
+print(config.get('Twitter', 'Twitter_Access_Key'))
 game_name = input("Please enter the game name (Use normal scene writing, do not include a dash or the release group):")
 release_group = input("Please enter the short form of the release group (do not include a dash): ")
 thread_link = input("Please enter the discussion thread link:")
