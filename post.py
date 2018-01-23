b684978a8af2134144491a3e74d8e5978a5609cc
raddcircles <raddcircles@gmail.com>
2018-01-22 20:38:58 -0500

    Added inputs

---
+++ post.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+from twitter import *
+
+game_name = input("Please enter the game name (Use normal scene writing, do not include a dash or the release group):")
+release_group = input("Please enter the short form of the release group (do not include a dash): ")
+thread_link = input("Please enter the discussion thread link:")
